ab4b4f9ad402d210a9df0f7edef6229bf34fcb9c
jorgeoliveira2099 <josuejorge2012@gmail.com>
2020-09-23 08:07:27 -0300

    alteração no filter

--- src/filter/Filter.java
+++ src/filter/Filter.java
@@ -28,15 +28,15 @@ public class Filter implements javax.servlet.Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		try {
-		chain.doFilter(request, response);
-		connection.commit();
+			chain.doFilter(request, response);
+			connection.commit();
 		}catch(Exception e) {
 			try {
+				e.printStackTrace();
 				connection.rollback();
 			} catch (SQLException e1) {				
 				e1.printStackTrace();
-			}
-			e.printStackTrace();
+			}			
 		}
 	}
 
